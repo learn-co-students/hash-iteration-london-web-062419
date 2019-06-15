6b8694aa2dbcd8d64a6c376be9165293429ea84a
github username <github email address>
2019-06-15 02:33:18 +0100

    each hash iteration

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,4 +1,6 @@
-# birthday_kids = {
+
+
+#  birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
 #   "Amanda" => 27
@@ -6,6 +8,24 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+ 
+  birthday_kids.each do |key, value|
+  puts "Happy Birthday #{key}! You are now #{value} years old!"
+  # use yield and if statment if kid is <12
+
+end
 end
 
+def age_appropriate_birthday(birthday_kids)
+  # add your code snippet here!
+ 
+  birthday_kids.each do |key, value|
+    if value <= 12
+  puts "Happy Birthday #{key}! You are now #{value} years old!"
+  # use yield and if statment if kid is <12
+    else 
+     puts "You are too old for this."
 
+end
+end
+end
